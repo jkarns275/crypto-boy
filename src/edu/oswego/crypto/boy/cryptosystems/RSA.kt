919dc93958f3d005fb3681116b584f218e745c35
Nicholas Esposito <nesposi3@oswego.edu>
2018-11-26 21:40:51 -0500

    I sure hope this one works

--- src/edu/oswego/crypto/boy/cryptosystems/RSA.kt
+++ src/edu/oswego/crypto/boy/cryptosystems/RSA.kt
@@ -1,4 +1,40 @@
 package edu.oswego.crypto.boy.cryptosystems
 
+import java.math.BigInteger
+import java.nio.ByteBuffer
+
 class RSA {
+    companion object {
+
+
+        fun encrypt(plaintext: String, publicKey: String): IntArray {
+            var public = publicKey.toString()
+            var splits = public.split(",")
+            val n = Integer.parseInt(splits[0])
+            val e = Integer.parseInt(splits[1])
+            var ciphertext = IntArray(plaintext.length)
+            var i = 0
+            for (byte in plaintext) {
+                val num = byte.toInt().toBigInteger()
+                var calc: BigInteger = num.pow(e).mod(n.toBigInteger())
+                ciphertext[i] = calc.toInt()
+                i++
+            }
+
+            var buff:ByteBuffer
+            return ciphertext
+        }
+        fun decrypt(ciphertext:IntArray, d:BigInteger, n:BigInteger ): ByteArray{
+            var plaintext = ByteArray(ciphertext.size)
+            var i = 0
+            for(num in ciphertext){
+                val bigNum = num.toBigInteger()
+                val numDec = bigNum.pow(d.toInt()).mod(n)
+                plaintext[i] = numDec.toByte()
+                i++
+
+            }
+            return plaintext
+        }
+    }
 }
